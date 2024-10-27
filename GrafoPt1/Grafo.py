96cd36aea3c7692fadd8d1645c22d440b31b92a4
Loildo Ferreira <loildo.ferreira@rehagro.edu.br>
2024-10-27 18:38:11 -0300

    grafo vazio, grafo completo, ponderacao e rotulo aresta, rotulo vertice

--- GrafoPt1/Grafo.py
+++ GrafoPt1/Grafo.py
@@ -4,13 +4,28 @@ class MatrizAdjacencia:
     def __init__(self, num_vertices):
         self.qtd_aresta = 0
         self.num_vertices = num_vertices
+        self.lista_rotulo_vertice = ["MG", "ES", "SP"]
         self.matriz = [[0] * num_vertices for i in range(num_vertices)]
+        self.matriz_rotulo_aresta = [[0] * num_vertices for i in range(num_vertices)]
 
-    def adicionar_aresta(self, v1, v2, ponderacao):
+    def adicionar_aresta_e_ponderar(self, v1, v2, ponderacao):
         self.qtd_aresta += 1 
         self.matriz[v1-1][v2-1] = ponderacao
         self.matriz[v2-1][v1-1] = ponderacao  # Para grafos não direcionados
+    
+    def rotular_aresta(self, v1, v2, rotulo):
+        self.matriz_rotulo_aresta[v1-1][v2-1] = rotulo
+        self.matriz_rotulo_aresta[v2-1][v1-1] = rotulo
 
+    def busca_nome_aresta(self, v1, v2):
+        for index, vertice in enumerate(self.lista_rotulo_vertice):
+            if(vertice == v1):
+                v1 = index
+            if(vertice == v2):
+                v2 = index
+        nome_aresta = self.matriz_rotulo_aresta[v1][v2]
+        print(nome_aresta)
+        
     def remover_aresta(self, v1, v2):
         self.qtd_aresta -= 1
         self.matriz[v1-1][v2-1] = 0
@@ -30,10 +45,32 @@ class MatrizAdjacencia:
         print(f"Quantidade de aresta: {self.qtd_aresta}")
 
     def exibir(self):
+        print(self.matriz[0])
         for index, linha in enumerate(self.matriz):
+            # print(f"{self.lista_rotulo_vertice[index]}  = {linha}")
             print(f"{index} = {linha}")
-            # print(linha)
 
+    def rotulo_vertice(self, rotulo):
+        self.lista_rotulo_vertice.append(rotulo)
+        print("Vertice rotulado")
+
+    def ponderar_vertice(self, ponderacao):
+        print("não tá feito!")
+
+    def lista_rotulo_vertices(self): 
+        print(self.lista_rotulo_vertice)
+
+    def grafo_completo(self):
+        resultado_formula = (self.num_vertices * (self.num_vertices - 1)) / 2
+        if(resultado_formula == self.qtd_aresta):
+            return print("É um grafo completo")
+
+        return print("Não é um grafo completo")
+
+    def grafo_vazio(self):
+        if(self.qtd_aresta == 0 & self.num_vertices != 1): 
+            return print("O grafo é um grafo vazio")
+        return print("O grafo possui pelo menos uma aresta")
 
 class MatrizIncidencia:
     def __init__(self, num_vertices, num_arestas):
--- GrafoPt1/teste_grafo.py
+++ GrafoPt1/teste_grafo.py
@@ -6,16 +6,42 @@ print("Matriz de Adjacência:")
 # qtd_vertices = int(input("Digite a qtd de vertices: "))
 # grafo_adj = MatrizAdjacencia(qtd_vertices)
 grafo_adj = MatrizAdjacencia(4)
-grafo_adj.adicionar_aresta(1, 2, 10)
+grafo_adj.adicionar_aresta_e_ponderar(1, 2, 10)
+grafo_adj.adicionar_aresta_e_ponderar(1, 3, 10)
+grafo_adj.adicionar_aresta_e_ponderar(1, 4, 10)
+grafo_adj.adicionar_aresta_e_ponderar(2, 3, 10)
+grafo_adj.adicionar_aresta_e_ponderar(2, 4, 10)
+grafo_adj.adicionar_aresta_e_ponderar(3, 4, 10)
+grafo_adj.exibir()
+print("")
+grafo_adj.checagem_quantidade_de_aresta()
+grafo_adj.grafo_completo()
+
+# rotular_vertice = int(input("Quer rotular os vertices?"))
+# if(rotular_vertice == 1): 
+#     for i in range(grafo_adj.num_vertices):
+#         rotulo = (input("nome vertice: "))
+#         grafo_adj.rotulo_vertice(rotulo)
+
+# grafo_adj.rotular_aresta(1,2, "E1\n")
+# grafo_adj.rotular_aresta(2,3, "aresta entre 2 e 3\n")
+# grafo_adj.busca_nome_aresta("MG","ES") # recebe o rotulo vertices e retorna o nome da aresta
+# grafo_adj.busca_nome_aresta("ES","SP") # recebe o rotulo vertices e retorna o nome da aresta
+# grafo_adj.lista_rotulo_vertices()
+exit()
+vertice_1 = int(input("Informe o primeiro vertice: "))
+vertice_2 = int(input("Informe o segundo vertice: "))
+grafo_adj.adicionar_aresta_e_ponderar(vertice_1, vertice_2)
+print(f"Aresta adicionada entre os vertices: {vertice_1} e {vertice_2}")
+
 
 grafo_adj.exibir()
-exit()
 
 qtd_arestas = int(input("Informe a qtd de arestas: "))
 for i in range(qtd_arestas):
     vertice_1 = int(input("Informe o primeiro vertice: "))
     vertice_2 = int(input("Informe o segundo vertice: "))
-    grafo_adj.adicionar_aresta(vertice_1, vertice_2)
+    grafo_adj.adicionar_aresta_e_ponderar(vertice_1, vertice_2)
     print(f"Aresta adicionada entre os vertices: {vertice_1} e {vertice_2}")
 
 remover = int(input("Quer remover arestas: \n - Digite 1 para remover\n - Digite 2 para não remover\n"))
@@ -34,7 +60,7 @@ else:
     grafo_adj.exibir()
 
     
-# grafo_adj.adicionar_aresta(2, 3)
+# grafo_adj.adicionar_aresta_e_ponderar(2, 3)
 # grafo_adj.exibir()
 # grafo_adj.remover_aresta(2, 3)
 # print("")
